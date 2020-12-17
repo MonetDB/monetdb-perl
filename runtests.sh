e5e3d6a3cfe925990fbe4e3fcce18364451db795
Joeri van Ruth <joeri.van.ruth@monetdbsolutions.com>
2020-12-17 14:09:17 +0100

    Also run the tests

--- runtests.sh
+++ runtests.sh
@@ -24,6 +24,16 @@ fi
 export DBI_DSN=dbi:monetdb:database=perltestdb
 export PERL5LIB=$PWD/MonetDB-CLI-MapiPP:$PWD/MonetDB-CLI:$PWD
 
+
+for i in DBD MonetDB-CLI/MonetDB MonetDB-CLI-MapiPP/MonetDB/CLI
+do
+	(
+		cd $i
+		perl Makefile.PL
+		make
+	)
+done
+
 make -C DBD test
 :
 make -C MonetDB-CLI/MonetDB test
