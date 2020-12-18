ff38203d28b9ffda6834bd0fd221a71e8a0f8b40
Joeri van Ruth <joeri.van.ruth@monetdbsolutions.com>
2020-12-18 10:53:15 +0100

    Run monetdb in a service container
    So we can later test against multiple versions of MonetDB
    by taking the image name from a matrix

--- runtests.sh
+++ runtests.sh
@@ -1,30 +1,10 @@
 #!/bin/bash
 
-DBPATH="$PWD"/DB
-
-
 set -e -x
 
-# Start the mserver
-rm -rf "$DBPATH"
-mkdir "$DBPATH"
-mserver5 --dbpath="$DBPATH/perltestdb" >"$DBPATH/mserver5.log" 2>&1 &
-pid=$!
-# Make sure to kill it when the script ends
-trap "kill -9 $pid 2>/dev/null" EXIT
+export DBI_DSN=dbi:monetdb:database=demo
 
-
-# After a few seconds, check if it's still running
-sleep 3
-if ! kill -0 $pid 2>/dev/null; then
-	echo "MSERVER STOPPED EARLY"
-	cat "$DBPATH/mserver5.log"
-	exit 1
-fi
-
-export DBI_DSN=dbi:monetdb:database=perltestdb
 export PERL5LIB=$PWD/MonetDB-CLI-MapiPP:$PWD/MonetDB-CLI:$PWD
-
 
 for i in DBD MonetDB-CLI/MonetDB MonetDB-CLI-MapiPP/MonetDB/CLI
 do
